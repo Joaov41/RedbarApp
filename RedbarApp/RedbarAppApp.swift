72edd45183b17dd49f9d56be28473913908c76d4
Joaov41 <52511571+Joaov41@users.noreply.github.com>
2025-05-08 10:38:05 +0100

    Initial Commit

---
+++ RedbarApp/RedbarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedbarAppApp.swift
+//  RedbarApp
+//
+//  Created by john val on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct RedbarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RedbarAppTests/RedbarAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RedbarAppTests.swift
+//  RedbarAppTests
+//
+//  Created by john val on 5/8/25.
+//
+
+import Testing
+@testable import RedbarApp
+
+struct RedbarAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
